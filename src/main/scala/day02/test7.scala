6d653ff59039669c0e24da0b11d4d170a9a6f3bb
Saisu <suai723@126.com>
2018-08-13 23:06:34 +0800

    20180813 23:06

---
+++ src/main/scala/day02/test7.scala
@@ -0,0 +1,14 @@
+package day02
+
+class test7 {
+
+}
+
+object test7{
+  def main(args: Array[String]): Unit = {
+     val a = "abc"
+    println(a.getClass)
+    println(classOf[List[Int]])
+
+  }
+}
